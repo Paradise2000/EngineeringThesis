80692b3003815a6aade450b2c701cea32166c4a8
Mikołaj Świetlik <102323850+Paradise2000@users.noreply.github.com>
2023-12-05 17:27:47 +0100

    small change

--- EngineeringThesisWebApp/src/script/opinion.js
+++ EngineeringThesisWebApp/src/script/opinion.js
@@ -1,10 +1,10 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const commentButton = document.getElementById('commentButton');
-//     const commentSection = document.getElementById('commentSection');
+document.addEventListener('DOMContentLoaded', function () {
+    const commentButton = document.getElementById('commentButton');
+    const commentSection = document.getElementById('commentSection');
 
-//     commentButton.addEventListener('click', function () {
-//         // Po kliknięciu przycisku pokaż sekcję komentarzy
-//         commentSection.style.display = commentSection.style.display === 'none' ? 'block' : 'none';
-//     });
-// });
+    commentButton.addEventListener('click', function () {
+        // Po kliknięciu przycisku pokaż sekcję komentarzy
+        commentSection.style.display = commentSection.style.display === 'none' ? 'block' : 'none';
+    });
+});
